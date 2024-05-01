53364d2ea3c0abfcd960e966cf575ac912c3ce55
nowmicac <nowmicac@gmail.com>
2024-04-30 23:25:07 -0400

    Initial Commit

---
+++ src/views/home.js
@@ -0,0 +1,185 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+import '../images/logo.jpg'
+import './home.css'
+
+
+const Home = (props) => {
+  const handleGalleryClick = () => {
+    // Add your logic to navigate to the pricing section
+    // For example, you can use window.scrollTo() or any routing library
+    const pricingElement = document.querySelector('.home-pricing');
+    pricingElement.scrollIntoView({ behavior: 'smooth' });
+    console.log('Gallery clicked!');
+  };
+
+
+
+const scrollToContact = () => {
+  // Find the element with the class 'home-pricing'
+  const pricingElement = document.querySelector('.home-banner');
+
+  console.log(pricingElement);
+  // Scroll the page to the top position of the pricing element
+  pricingElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (
+    <div className="home-container">
+      <Helmet>
+        <title>Sugar Sprinkle Delights</title>
+      </Helmet>
+      <div className="home-header">
+        <header
+          data-thq="thq-navbar"
+          className="home-navbar-interactive navbarContainer"
+        >
+          <div data-thq="thq-navbar-nav" className="home-desktop-menu">
+            <img
+              src={require('../images/logo.jpg')}
+              alt="image"
+              className="home-image"
+            />
+            <nav className="home-links">
+              <a href='#gallery' className="home-nav42 bodySmall">Gallery</a>
+              <a href= '#contact' className="home-nav52 bodySmall">Contact</a>
+            </nav>
+          </div>
+          <div data-thq="thq-burger-menu" className="home-burger-menu">
+            <svg viewBox="0 0 1024 1024" className="home-icon socialIcons">
+              <path d="M128 554.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 298.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667zM128 810.667h768c23.552 0 42.667-19.115 42.667-42.667s-19.115-42.667-42.667-42.667h-768c-23.552 0-42.667 19.115-42.667 42.667s19.115 42.667 42.667 42.667z"></path>
+            </svg>
+          </div>
+          <div
+            data-thq="thq-mobile-menu"
+            className="home-mobile-menu1 mobileMenu"
+          >
+            <div className="home-nav">
+              <div className="home-top">
+                <span className="logo">DELIGHTS</span>
+                <div data-thq="thq-close-menu" className="home-close-menu">
+                  <svg
+                    viewBox="0 0 1024 1024"
+                    className="home-icon02 socialIcons"
+                  >
+                    <path d="M810 274l-238 238 238 238-60 60-238-238-238 238-60-60 238-238-238-238 60-60 238 238 238-238z"></path>
+                  </svg>
+                </div>
+              </div>
+              <nav className="home-links1">
+                <a href='#gallery' className="home-nav421 bodySmall" onClick={handleGalleryClick}>Gallery</a>
+                <a href= '#contact' className="home-nav521 bodySmall" onClick={scrollToContact}>Contact</a>
+              </nav>
+            </div>
+            <div>
+              <svg
+                viewBox="0 0 950.8571428571428 1024"
+                className="home-icon04 socialIcons"
+              >
+                <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 877.7142857142857 1024"
+                className="home-icon06 socialIcons"
+              >
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 602.2582857142856 1024"
+                className="home-icon08 socialIcons"
+              >
+                <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+              </svg>
+            </div>
+          </div>
+        </header>
+      </div>
+      <div className="home-hero"></div>
+      <div className="home-features"></div>
+      <div id="gallery" className="home-pricing">
+        <div className="home-pricing1 pricingContainer">
+          <div className="home-container1">
+            <div className="home-pricing-card basicPricingCard"></div>
+            <div className="home-pricing-card01 freePricingCard"></div>
+            <div className="home-pricing-card02 basicPricingCard"></div>
+          </div>
+          <div className="home-container2">
+            <div className="home-pricing-card03 basicPricingCard"></div>
+            <div className="home-pricing-card04 freePricingCard"></div>
+            <div className="home-pricing-card05 basicPricingCard"></div>
+          </div>
+          <div className="home-container3">
+            <div className="home-pricing-card06 basicPricingCard"></div>
+            <div className="home-pricing-card07 freePricingCard"></div>
+            <div className="home-pricing-card08 basicPricingCard"></div>
+          </div>
+          <div className="home-container4">
+            <div className="home-pricing-card09 basicPricingCard"></div>
+            <div className="home-pricing-card10 freePricingCard"></div>
+            <div className="home-pricing-card11 basicPricingCard"></div>
+          </div>
+        </div>
+      </div>
+      <div id ="contact" className="home-banner"></div>
+      <div className="home-faq">
+        <div className="home-faq-container faqContainer">
+          <h1 className="home-text">Contact Us</h1>
+          <form className="home-form">
+            <div className="home-container5">
+              <div className="home-container6">
+                <input
+                  type="text"
+                  placeholder="Name"
+                  required="true"
+                  className="home-textinput input"
+                />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  required="true"
+                  className="home-textinput1 input"
+                />
+              </div>
+              <button type="button" className="home-button button">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div className="home-footer">
+        <footer className="home-footer1 footerContainer">
+          <div className="home-container7"></div>
+          <div className="home-container8">
+            <span className="bodySmall home-text1">
+              © 2023 Sugar Sprinkle Delights, All Rights Reserved.
+            </span>
+            <div className="home-icon-group1">
+              <svg
+                viewBox="0 0 950.8571428571428 1024"
+                className="home-icon10 socialIcons"
+              >
+                <path d="M925.714 233.143c-25.143 36.571-56.571 69.143-92.571 95.429 0.571 8 0.571 16 0.571 24 0 244-185.714 525.143-525.143 525.143-104.571 0-201.714-30.286-283.429-82.857 14.857 1.714 29.143 2.286 44.571 2.286 86.286 0 165.714-29.143 229.143-78.857-81.143-1.714-149.143-54.857-172.571-128 11.429 1.714 22.857 2.857 34.857 2.857 16.571 0 33.143-2.286 48.571-6.286-84.571-17.143-148-91.429-148-181.143v-2.286c24.571 13.714 53.143 22.286 83.429 23.429-49.714-33.143-82.286-89.714-82.286-153.714 0-34.286 9.143-65.714 25.143-93.143 90.857 112 227.429 185.143 380.571 193.143-2.857-13.714-4.571-28-4.571-42.286 0-101.714 82.286-184.571 184.571-184.571 53.143 0 101.143 22.286 134.857 58.286 41.714-8 81.714-23.429 117.143-44.571-13.714 42.857-42.857 78.857-81.143 101.714 37.143-4 73.143-14.286 106.286-28.571z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 877.7142857142857 1024"
+                className="home-icon12 socialIcons"
+              >
+                <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
+              </svg>
+              <svg
+                viewBox="0 0 602.2582857142856 1024"
+                className="home-icon14 socialIcons"
+              >
+                <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+              </svg>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </div>
+  )
+}
+
+export default Home
